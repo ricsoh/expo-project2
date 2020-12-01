7bac821e1012dc3f3562f6321ab4143c65b558a1
ricsoh <ricsoh@gmail.com>
2020-12-01 17:59:22 +0800

    add screen touch

--- App.js
+++ App.js
@@ -82,18 +82,6 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    
-    shadowColor: 'rgba(0,0,0, .4)', // IOS
-    shadowOffset: { height: 1, width: 1 }, // IOS
-    shadowOpacity: 1, // IOS
-    shadowRadius: 1, //IOS
-    elevation: 2, // Android
-    height: 50,
-    width: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    
     backgroundColor: 'lightblue',
     padding: 10,
     borderRadius: 15,
