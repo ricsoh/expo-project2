93aee0bd5022aa9f3ae4d5b060893375762ea108
ricsoh <ricsoh@gmail.com>
2020-12-01 12:28:12 +0800

    Added buttons

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, Button, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    if(count > 0){
+      setCount(count - 1);
+    }else{
+      alert("Reached 0!")
+    }
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{count}</Text>
+      <Button title='Increment' onPress={increment}></Button>
+      <Button title='Decrement' onPress={decrement}></Button>
+      <Button title='Reset!' onPress={reset}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +38,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 20,
   },
 });
